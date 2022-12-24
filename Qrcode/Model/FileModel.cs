4bc3ad5b23d9c9dd296943d2e4fdd3fffaa0c19e
Wissam Farhat <WissamFarhat@angrymonkeyagency.com>
2022-12-24 14:41:26 +0200

    creat project

---
+++ Qrcode/Model/FileModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Qrcode.Model
+{
+    public class FileModel
+    {
+        public string Base64data { get; set; }
+        public string ContentType { get; set; }
+        public string FileName { get; set; }
+    }
+}
